4c1ae1525ff391661b27abbf8e2b0cd9b4c1510d
Jack Mwangi <JackMwangi@Jack.local>
2016-07-22 13:18:17 +0300

    (Feature) Login interface

--- picfect/src/app/auth/auth.component.ts
+++ picfect/src/app/auth/auth.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference path='../../../typings/fbsdk/fbsdk.d.ts'/>
+/// <reference path='../../../typings/jquery/jquery.d.ts'/>
+
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
+import {Auth} from './auth.service';
+declare var $: JQueryStatic;
 
 @Component({
   moduleId: module.id,
   selector: 'app-auth',
   templateUrl: 'auth.component.html',
-  styleUrls: ['auth.component.css']
+  styleUrls: ['auth.component.css'],
+  providers: [Auth]
 })
-export class AuthComponent implements OnInit {
-
-  constructor() {}
+export class AuthComponent implements OnInit, AfterViewInit {
+  @ViewChild('appname') el: ElementRef;
+  constructor() { }
 
   ngOnInit() {
+    FB.init(
+      {
+        appId: '1622731728039944',
+        xfbml: true,
+        version: 'v2.0'
+      }
+    );
   }
-
+  ngAfterViewInit() {
+    $(this.el.nativeElement).delay(1000).animate({ "opacity": "1" }, 1700);
+  }
+  static loginResponse(response: any) {
+    status = response['status'];
+    if (status == 'connected') {
+      let access_token = response['authResponse']['accessToken'];
+      localStorage.setItem('id_token', access_token);
+    }
+  }
+  login() {
+    FB.login(function (response) {
+      console.log(response);
+      AuthComponent.loginResponse(response);
+    });
+  }
 }
